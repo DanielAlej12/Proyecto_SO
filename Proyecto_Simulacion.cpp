19888ebc11cbfae3aa505fcb6cedbad469a9de97
Daniel Quintero <danialej2006@gmail.com>
2025-12-02 22:07:12 -0400

    Función runMultipleTimes
    Esta función ejecuta un algoritmo (FIFO, LIFO, RR) múltiples veces (por defecto 1000 iteraciones) sobre las mismas actividades para obtener resultados estadísticamente estables. El principal objetivo es medir el tiempo total con chrono y devolver el tiempo de ejecución promedio en segundos.

--- Proyecto_Simulacion.cpp
+++ Proyecto_Simulacion.cpp
@@ -33,3 +33,37 @@ struct SimulationResult {
     double avg_I;
     double executionTime; // Tiempo de ejecución promedio por iteración
 };
+
+// --- FUNCIÓN AUXILIAR: EJECUTAR MÚLTIPLES VECES ---
+SimulationResult runMultipleTimes(function<SimulationResult(vector<Activity>)> algorithm, 
+                                  const vector<Activity>& activities, int iterations = 1000) {
+    vector<SimulationResult> results;
+    
+    auto START = chrono::high_resolution_clock::now();
+
+    for(int i = 0; i < iterations; i++) {
+        SimulationResult single_result = algorithm(activities);
+        results.push_back(single_result);
+    }
+    
+    auto END = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = END - START;
+    
+    // Promediar métricas
+    SimulationResult avg_result;
+    avg_result.completedActivities = results[0].completedActivities; // Usar primera iteración
+    double total_T = 0, total_E = 0, total_I = 0;
+    
+    for(const auto& res : results) {
+        total_T += res.avg_T;
+        total_E += res.avg_E;
+        total_I += res.avg_I;
+    }
+    
+    avg_result.avg_T = total_T / iterations;
+    avg_result.avg_E = total_E / iterations;
+    avg_result.avg_I = total_I / iterations;
+    avg_result.executionTime = duration.count() / iterations; // Tiempo promedio
+    
+    return avg_result;
+}
